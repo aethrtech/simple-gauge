682d6eb020e1b99a304b1fa3f97d9c8d606a9ab2
farhadfarhadii <farhadfarhadii@users.noreply.github.com>
2019-10-22 01:19:02 +0100

    added test launcher script

---
+++ scripts/spawn-server.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process'),
+{ resolve } = require('path')
+
+const programs = [{
+    name:'PARCEL',
+    cmd: 'node', 
+    args: [resolve('.','scripts','pre-test')]
+},{
+    name:'CYPRESS',
+    cmd:'npx',
+    args: ['cypress','open']
+}]
+
+for (let program of programs){
+    
+    console.log(`execing program: ${program.name}`)
+
+    let p = exec(`${program.cmd} ${program.args.join(' ')}`,(err, stdout,stderr) => {
+
+        if (err) console.error(`\u001b[30;1m${err}\u001b[0m`)
+        if(stdout) console.log(`\u001b[32;1m[${program.name}]: ${Buffer.from(stdout).toString()}\u001b[0m`)
+        if (stderr) console.error(`\u001b[30;1m${stderr}\u001b[0m`)
+    })
+
+    p.on('close', () => process.exit(0))
+
+}
