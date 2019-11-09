009c8dc24988305c788b4ac2cec65e24953f9bf3
farhadfarhadii <farhadfarhadii@users.noreply.github.com>
2019-11-09 01:15:38 +0000

    cleanup

--- scripts/spawn-server.js
+++ scripts/spawn-server.js
@@ -1,11 +1,10 @@
-const { resolve } = require('path')
-
 const cy = require('cypress')
 const build = require('./build-test')
 const server = require('../e2e/server')
 
 build(function(){
-    cy.open()
     server()
+    cy.open()
+    
 })
 
