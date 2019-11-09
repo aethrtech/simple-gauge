8b6f4e8f3415f04aabd15184a4c7d0e4deded773
farhadfarhadii <farhadfarhadii@users.noreply.github.com>
2019-11-09 07:16:56 +0000

    removed create.ts

--- src/modules/create.ts
+++
@@ -1,9 +0,0 @@
-import init from './init'
-
-
-export default function Create(container:Container,options?,cb?:Function):void{
-    init(container, options, err => {
-        if (err) throw new Error(err)
-        return cb()
-    })
-}
