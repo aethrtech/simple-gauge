48d4fa96a5fa510aa81da3321655de7b86a89176
farhadfarhadii <farhadfarhadii@users.noreply.github.com>
2019-11-09 01:15:50 +0000

    updated pathing

--- e2e/server.js
+++ e2e/server.js
@@ -1,27 +1,23 @@
 module.exports = function(){
     const http = require('http'),
     fs = require('fs'),
-    options = {
-        host:'localhost',
-        port:8080
-    },
     { resolve } = require('path')
 
     http.createServer(function(req,res){
         switch(req.url){
             case '/':
                 res.setHeader('Content-Type','text/html')
-                var rs = fs.createReadStream(resolve('e2e','./index.html'))
+                fs.createReadStream(resolve('e2e','./index.html'))
                 .pipe(res)
                 break
-            case '/index.js':
+            case '/scripts/index.js':
                     res.setHeader('Content-Type','text/javascript')
-                    var rs = fs.createReadStream(resolve('e2e','scripts','index.js'))
+                    fs.createReadStream(resolve('e2e','scripts','index.js'))
                     .pipe(res)
                 break
-            case './index.js.map':
+            case '/scripts/index.js.map':
                     res.setHeader('Content-Type','text/pain')
-                    var rs = fs.createReadStream(resolve('e2e','scripts','index.js.map'))
+                    fs.createReadStream(resolve('e2e','scripts','index.js.map'))
                     .pipe(res)
 
         }
